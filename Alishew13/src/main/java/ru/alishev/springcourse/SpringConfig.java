d1de6195ff1f3ef2886cc20db849e38c1688b88a
OdinSGate <andrey.whiteid@gmail.com>
2022-11-20 17:41:21 +0300

    Ожидает ревью.

---
+++ Alishew13/src/main/java/ru/alishev/springcourse/SpringConfig.java
@@ -0,0 +1,44 @@
+package ru.alishev.springcourse;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Neil Alishev
+ */
+@Configuration
+@PropertySource("classpath:musicPlayer.properties")
+public class SpringConfig {
+    @Bean
+    public ClassicalMusic classicalMusic() {
+        return new ClassicalMusic();
+    }
+
+    @Bean
+    public RockMusic rockMusic() {
+        return new RockMusic();
+    }
+
+    @Bean
+    public RapMusic rapMusic() {
+        return new RapMusic();
+    }
+
+    public List<Music> musicList(){
+        return Arrays.asList(classicalMusic(), rapMusic(), rockMusic());
+    }
+
+    @Bean
+    public MusicPlayer musicPlayer() {
+        return new MusicPlayer(musicList());
+    }
+
+    @Bean
+    public Computer computer() {
+        return new Computer(musicPlayer());
+    }
+}
